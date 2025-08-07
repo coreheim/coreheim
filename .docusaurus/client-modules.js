434a18018898ef6f6ec67fe584f8bda3dfc3f400
coreheim <coreheim>
2025-08-07 10:25:46 +1000

    remove docusarus

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/colby/colby/coreheim/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/colby/colby/coreheim/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/colby/colby/coreheim/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/colby/colby/coreheim/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/colby/colby/coreheim/src/css/custom.css"),
-];
